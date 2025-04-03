3c80f426e191ae4bb5655113632a8c3b5ccebb3d
pranusri1903 <pranu.sri1903@gmail.com>
2025-04-03 22:36:40 +0530

    CP Day2

--- Arrays/Start.java
+++ Arrays/Start.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Here We Go!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int c= sc.nextInt();
+            int b = sc.nextInt();
+
+            if (a + c + b >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
